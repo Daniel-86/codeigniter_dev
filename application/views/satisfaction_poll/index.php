d46700f73556425e7f0f0919a0264455cf95acd1
Daniel Jiménez <daniel.jimenez@scitum.com.mx>
2015-09-14 20:30:37 -0500

    first commit

---
+++ application/views/satisfaction_poll/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Codeigniter dev env</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.css">
+</head>
+<body ng-app="rootModule">
+
+<div class="container">
+    <form novalidate name="loadForm">
+
+    </form>
+</div>
+
+<script src="/assets/js/angular.js"></script>
+<script src="/assets/js/app/app.js"></script>
+</body>
---
+++ application/views/satisfaction_poll/load_data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: daniel.jimenez
+ * Date: 14/09/2015
+ * Time: 07:28 PM
+ */
